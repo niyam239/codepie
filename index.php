8518759ce538b49e1d6902d5c6b49952fc32d313
Niyam Muliya <niyam239@gmail.com>
2022-03-13 13:16:40 +0530

    mentors photo added

--- index.php
+++ index.php
@@ -449,7 +449,7 @@
                      <!--person column-->
                      <div class="col-md-4 mb-4">
                
-                        <img class="rounded-circle" alt="100x100" src="./images/avatar.jpg"
+                        <img class="rounded-circle" alt="100x100" src="./images/team/krish.jpeg"
                           data-holder-rendered="true" style="height: 250px; width: 250px">
                           <h2 class="mt-2 p-0">Krish Shewani</h2>
                         <p class="mt-0" >Mentor</p>
@@ -461,6 +461,15 @@
                         <img class="rounded-circle" alt="100x100" src="./images/team/jainil.jpeg"
                           data-holder-rendered="true" style="height: 250px; width: 250px">
                           <h2 class="mt-2 p-0">Jainil Rana</h2>
+                        <p class="mt-0" >Mentor</p>
+                      </div>
+                      <!--person column-->
+                      <!--person column-->
+                     <div class="col-md-4 mb-4">
+               
+                        <img class="rounded-circle" alt="100x100" src="./images/team/raj.jpg"
+                          data-holder-rendered="true" style="height: 250px; width: 250px">
+                          <h2 class="mt-2 p-0">Raj Mangukiya</h2>
                         <p class="mt-0" >Mentor</p>
                       </div>
                       <!--person column-->
